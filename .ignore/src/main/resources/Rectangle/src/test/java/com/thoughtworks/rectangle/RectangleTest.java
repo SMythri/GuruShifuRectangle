6324c3265864454f36ea31e920ca4b62747021c5
SMythri <mythrisrinivas2000@gmail.com>
2022-07-29 09:01:21 +0000

    added methods to calculate recangle area and perimeter and tested

---
+++ .ignore/src/main/resources/Rectangle/src/test/java/com/thoughtworks/rectangle/RectangleTest.java
@@ -0,0 +1,28 @@
+package com.thoughtworks.rectangle;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.closeTo;
+
+
+
+public class RectangleTest {
+    @Test
+    void shouldReturnAreaWhenLengthAndBreadthIsGiven(){
+
+        final Rectangle rectangle = new Rectangle(2.4,3.6);
+        final double area = rectangle.area();
+
+        assertThat(area, is(closeTo(8.64,0.001)));
+
+    }
+
+    @Test
+    void shouldReturnPerimeterWhenLengthAndBreadthIsGiven(){
+
+        final Rectangle rectangle = new Rectangle(2.67,9.36);
+        final double perimeter = rectangle.perimeter();
+        assertThat(perimeter, is(closeTo(24.06,0.001)));
+    }
+}
